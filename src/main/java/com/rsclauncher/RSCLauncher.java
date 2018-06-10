1c1dbb7c855396f88d761bbfa01c5488e091d941
Forest Belton <forest@homolo.gy>
2018-06-10 13:50:44 -0700

    Add menu item for dumping z vars

--- src/main/java/com/rsclauncher/RSCLauncher.java
+++ src/main/java/com/rsclauncher/RSCLauncher.java
@@ -5,28 +5,12 @@ import com.rsclauncher.menu.MenuBarFactory;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Base64;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JMenuBar;
 
 public class RSCLauncher {
-
-  private static String decryptString(String s, char[] key) {
-    final char[] out = s.toCharArray();
-
-    for (int i = 0; i < out.length; ++i) {
-      out[i] = (char)(out[i] ^ key[i % key.length]);
-    }
-
-    return new String(out);
-  }
 
   public static class ClientDebugThread extends Thread {
 
@@ -42,37 +26,6 @@ public class RSCLauncher {
       while (true) {
         try {
           Thread.sleep(1000);
-/*
-          final Field ChField = client.getClass().getDeclaredField("Ch");
-
-          ChField.setAccessible(true);
-          final Object Ch = ChField.get(client);
-          final
-
-          final StringBuilder output = new StringBuilder();
-
-          if (Ch == null) {
-            continue;
-          }
-
-          String theseBytes = Base64.getEncoder().encodeToString(vk);
-          if (lastBytes != null && lastBytes.equals(theseBytes)) {
-            continue;
-          }
-
-          lastBytes = theseBytes;
-          output.append("===\n");
-
-          for (int i = 0; i < vk.length; ++i) {
-            output.append(String.format("%x", Byte.toUnsignedInt(vk[i])));
-
-            if (i != vk.length - 1) {
-              output.append(' ');
-            }
-          }
-
-          output.append('\n');
-          Files.write(Paths.get("/Users/case/Projects/rsclauncher/packet.log"), output.toString().getBytes(), StandardOpenOption.APPEND);*/
         } catch (Exception ex) {
           ex.printStackTrace();
         }
@@ -128,33 +81,6 @@ public class RSCLauncher {
     // Load client instance
     final Class<?> clientClass = classLoader.loadClass("client");
     final Object client = clientClass.newInstance();
-
-    try {
-      final Field[] clientFields = client.getClass().getDeclaredFields();
-
-      for (Field clientField : clientFields) {
-        if (!clientField.getType().isAssignableFrom(byte[].class)) {
-          continue;
-        }
-
-        if (Modifier.isStatic(clientField.getModifiers())) {
-          continue;
-        }
-
-        /*clientField.setAccessible(true);
-        final int fieldValue = clientField.getInt(client);
-
-        if (fieldValue == 912) {
-          System.out.println("axisRotation: " + clientField.getName());
-          break;
-        }*/
-
-        System.out.println("byte[] " + clientField.getName());
-      }
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-
     final Client clientObject = Client.class.cast(client);
 
     // Set up menu
---
+++ src/main/java/com/rsclauncher/menu/DumpZVars.java
@@ -0,0 +1,95 @@
+package com.rsclauncher.menu;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+public class DumpZVars implements MenuItem {
+
+  private final ClassLoader classLoader;
+
+  public DumpZVars(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
+  @Override
+  public String title() {
+    return "Dump Z vars";
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    try {
+      final JarInputStream jarInputStream = new JarInputStream(new FileInputStream(new File("rsclassic.jar")));
+      final List<String> classFiles = new ArrayList<>();
+
+      JarEntry entry;
+      while ((entry = jarInputStream.getNextJarEntry()) != null) {
+        final String entryName = entry.getName();
+
+        if (entryName.endsWith(".class")) {
+          classFiles.add(entryName);
+        }
+      }
+
+      final JsonFactory factory = new JsonFactory();
+      final JsonGenerator generator = factory.createGenerator(System.out);
+
+      generator.writeStartObject();
+
+      for (String fileName : classFiles) {
+        final String className = fileName.replaceFirst("\\.class$", "");
+
+        // these doesn't like being loaded for whatever reason, seems to try and import
+        // microsoft-specific packages
+        // TODO: Handle on classloader side of things
+        if (Arrays.asList("h", "ha", "mb", "n", "tb", "u").contains(className)) {
+          continue;
+        }
+
+        try {
+          final Class<?> clazz = classLoader.loadClass(className);
+          final Field zField = clazz.getDeclaredField("z");
+
+          if (!zField.getType().isAssignableFrom(String[].class)) {
+            continue;
+          }
+
+          zField.setAccessible(true);
+          final String[] z = (String[]) zField.get(null);
+
+          generator.writeArrayFieldStart(className);
+          for (String zString : z) {
+            generator.writeString(zString);
+          }
+          generator.writeEndArray();
+        } catch (NoSuchFieldException ex) {}
+      }
+
+      generator.writeEndObject();
+      generator.flush();
+      System.out.println("");
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  private static String decryptString(String s, char[] key) {
+    final char[] out = s.toCharArray();
+
+    for (int i = 0; i < out.length; ++i) {
+      out[i] = (char)(out[i] ^ key[i % key.length]);
+    }
+
+    return new String(out);
+  }
+}
